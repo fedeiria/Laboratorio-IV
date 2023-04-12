63ba6a345e671df7661e31ca8fbcb80850bb4260
fedeiria <fedeiria@gmail.com>
2023-04-11 21:07:31 -0300

    first commit

---
+++ ejercicio2/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ejercicio 2';
+}
+
+class User {
+  name: String = "";
+  password: String = "";
+}
